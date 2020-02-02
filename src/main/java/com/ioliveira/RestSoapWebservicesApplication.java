6a69126122e5c9b83b4dfbd4732c6329816f55ec
Igor Oliveira <igorfoliveira@gmail.com>
2020-02-02 14:33:40 -0300

    projeto criado

---
+++ src/main/java/com/ioliveira/RestSoapWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.ioliveira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSoapWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestSoapWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ioliveira/RestSoapWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ioliveira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSoapWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
