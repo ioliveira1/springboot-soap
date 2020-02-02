12b49fe9be974afe91ad44746601d4d2c0f90b52
Igor Oliveira <igorfoliveira@gmail.com>
2020-02-02 17:41:32 -0300

    configurando Message Dispatcher Servlet

---
+++ src/main/java/com/ioliveira/config/WebServiceConfig.java
@@ -0,0 +1,12 @@
+package com.ioliveira.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.ws.config.annotation.EnableWs;
+
+@EnableWs
+@Configuration
+public class WebServiceConfig {
+	
+	
+
+}
