1979619f9136e3447b775e40a4565a2db61b9d76
Igor Oliveira <igorfoliveira@gmail.com>
2020-02-02 16:08:50 -0300

    configurando endpoint para request

---
+++ src/main/java/com/ioliveira/endpoints/CourseDetailsEndpoint.java
@@ -0,0 +1,29 @@
+package com.ioliveira.endpoints;
+
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.ioliveira.courses.CourseDetails;
+import com.ioliveira.courses.GetCourseDetailsRequest;
+import com.ioliveira.courses.GetCourseDetailsResponse;
+
+@Endpoint
+public class CourseDetailsEndpoint {
+	
+	@PayloadRoot(namespace = "http://ioliveira.com/courses", localPart = "GetCourseDetailsRequest")
+	@ResponsePayload
+	public GetCourseDetailsResponse getCourse(@RequestPayload GetCourseDetailsRequest request) {
+		GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+		
+		CourseDetails cd = new CourseDetails();
+		cd.setId(request.getId());
+		cd.setName("Microservices with SOAP");
+		cd.setDescription("Learn spring-boot with SOAP");
+		response.setCourseDetails(cd);
+		
+		return response;
+	}
+
+}
