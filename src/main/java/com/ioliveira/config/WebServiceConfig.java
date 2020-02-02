77bf5d3c86312e7810f48c24715241e02759f4d2
Igor Oliveira <igorfoliveira@gmail.com>
2020-02-02 18:02:44 -0300

    adicionando as definicoes do wsdl na configuracao

--- src/main/java/com/ioliveira/config/WebServiceConfig.java
+++ src/main/java/com/ioliveira/config/WebServiceConfig.java
@@ -1,12 +1,45 @@
 package com.ioliveira.config;
 
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
+import org.springframework.xml.xsd.SimpleXsdSchema;
+import org.springframework.xml.xsd.XsdSchema;
 
 @EnableWs
 @Configuration
 public class WebServiceConfig {
 	
+	@Bean
+	public ServletRegistrationBean<?> messageDispatcher(ApplicationContext context) {
+		MessageDispatcherServlet mds = new MessageDispatcherServlet();
+		
+		mds.setApplicationContext(context);
+		mds.setTransformWsdlLocations(true);
+		
+		return new ServletRegistrationBean<>(mds, "/ws/*");
+	}
 	
+	@Bean(name = "courses")
+	public DefaultWsdl11Definition wsdlDefinition(XsdSchema schema) {
+		DefaultWsdl11Definition definition = new DefaultWsdl11Definition();
+		
+		definition.setPortTypeName("CoursePort");
+		definition.setTargetNamespace("http://ioliveira.com/courses");
+		definition.setLocationUri("/ws");
+		definition.setSchema(schema);
+		
+		return definition;
+	}
+	
+	@Bean
+	public XsdSchema courseSchema() {
+		return new SimpleXsdSchema(new ClassPathResource("course-details.xsd"));
+	}
 
 }
